83a3193e6861bb9d081b58986e0e534c6378b69c
Manu Kanchiraju <mckanchiraju@gmail.com>
2017-01-22 01:08:07 +0000

    Fixed some bugs while adding Mailer settings to environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-ActionMailer::Base.smtp_setting = {
-    :port               => ENV['MAILGUN_SMTP_PORT'],
-    :address            => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name          => ENV['MAILGUN_SMTP_LOGIN'],
-    :password           => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain             => 'ancient-savannah-15778.herokuapp.com'
-    :authentication     => :plain,
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'ancient-savannah-15778.herokuapp.com',
+  :authentication => :plain,
 }
-
 ActionMailer::Base.delivery_method = :smtp
